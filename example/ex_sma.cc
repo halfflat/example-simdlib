5334d406577ced35ab5b4d3b0859b74dfc9fe998
Sam Yates <sam@quux.dropbear.id.au>
2019-10-14 16:58:33 +0200

    Add 'generic' cases to ex_sma

--- example/ex_sma.cc
+++ example/ex_sma.cc
@@ -11,9 +11,31 @@ void sma(double* __restrict c, double k, const double* a, const double* b) {
 }
 
 using namespace tinysimd;
-using vdouble = simd<double, 4, abi::avx2>;
 
 void sma_avx2(double* __restrict c, double k, const double* a, const double* b) {
+    using vdouble = simd<double, 4, abi::avx2>;
+
+    for (unsigned i = 0; i<N; i += vdouble::width) {
+        vdouble va(a+i), vb(b+i);
+        vdouble vc = k*va + vb;
+        vc.copy_to(c+i);
+    }
+}
+
+
+void sma_generic1(double* __restrict c, double k, const double* a, const double* b) {
+    using vdouble = simd<double, 1, abi::generic>;
+
+    for (unsigned i = 0; i<N; i += vdouble::width) {
+        vdouble va(a+i), vb(b+i);
+        vdouble vc = k*va + vb;
+        vc.copy_to(c+i);
+    }
+}
+
+void sma_generic4(double* __restrict c, double k, const double* a, const double* b) {
+    using vdouble = simd<double, 4, abi::generic>;
+
     for (unsigned i = 0; i<N; i += vdouble::width) {
         vdouble va(a+i), vb(b+i);
         vdouble vc = k*va + vb;
